10d6ab9f7c259ea43230f18df89a251f9738874d
asp <ahmtsytplt@gmail.com>
2019-06-09 18:14:57 +0300

    ASP Resources first

--- Resource.h
+++ Resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by Fore.rc
+// Microsoft Visual C++ içerme dosyasý oluþturdu.
+// Fore.rc tarafýndan kullanýlmýþ
 //
 #define IDB_GRASS2                      102
 #define IDB_GRASS3                      103
@@ -26,6 +26,15 @@
 #define IDB_WARRIORL                    129
 #define IDB_BITMAP1                     131
 #define IDB_TARGET                      132
+#define IDB_WARRIOR_LA1                 133
+#define IDB_WARRIOR_LA2                 134
+#define IDB_WARRIOR_LW1                 135
+#define IDB_WARRIOR_LW2                 136
+#define IDB_WARRIOR_RA1                 137
+#define IDB_WARRIOR_RA2                 138
+#define IDB_WARRIOR_RW1                 139
+#define IDB_BITMAP4                     140
+#define IDB_WARRIOR_RW2                 140
 #define IDI_FORE                        1000
 #define IDI_FORE_SM                     1001
 #define IDB_FOREST                      2000
@@ -35,7 +44,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        141
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           101
