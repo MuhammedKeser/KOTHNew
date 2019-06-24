a8822fd2a99e5ce6c7bbd8ea02a4cf34d0abd6dc
Momo <momo.dadick@yandex.com>
2019-06-24 20:43:09 +0300

    Test out unit.h modification

--- Unit.h
+++ Unit.h
@@ -97,7 +97,7 @@ public:
 	void PreventOverlap(Sprite* otherSprite);
 	virtual void          Draw(HDC hDC, Camera* cam) override;
 	void DrawHealthBar(HDC hDC, Camera* cam);
-	int GetStatus() { return status; };
+	int GetStatus() { return status; }; 
 	void SetStatus(UNIT_STATUS statusToSet) { status = statusToSet; };
 	int GetHealth() { return m_health; };
 	void SetHealth(int newHealth) { m_health = newHealth; };
